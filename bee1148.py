892f034e9edbc39ab998f5bac70ad4d9be1334b0
Leonardo Azzi Martins <leoazzim@gmail.com>
2024-01-17 15:21:58 -0300

    1148: processa a entrada e constroi a saida do problema

--- bee1148.py
+++ bee1148.py
@@ -31,7 +31,8 @@ def search(graph):
     for i in graph.keys():
         visited[i]=False
 
-# update_weight: busca o vértice u como chave no dicionário e busca o vértice v como item 
+# update_weight: Any, Any, int -> Dict
+# Busca o vértice u como chave no dicionário e busca o vértice v como item 
 # na lista de valores. Atualiza o w da lista.
 def update_weight(u, v, new_weight):
     for idx, edge in enumerate(graph[u]):
@@ -39,29 +40,27 @@ def update_weight(u, v, new_weight):
         if neighbour == v:
             graph[u][idx][1] = new_weight
 
-# verify_cycles: verifica se existe um ciclo dentro do grafo e atualiza o peso
+# verify_cycles: Dict -> Dict
+# Verifica se existe um ciclo dentro do grafo e atualiza o peso
 # das arestas
 def verify_cycles(graph):
-    print(graph)
     # para cada vértice
-    for i in graph.keys():
-        print("Vértice ", i)
+    # for i in graph.keys():
+    for i in list(graph):
         
         # para cada vizinho do vértice atual e peso da aresta
         for n, w in graph[i]:
-            print(" Vizinho ", graph[i])
 
             # para cada sucessor do vizinho e respectivo peso
             for nn, ww in graph[n]:
-                print("     Sucessor do vizinho ", n, " é ", nn, " em ", graph[n])
-
+                
                 # se o sucessor do vizinho for o próprio vértice atual
                 if nn == i:
-                    print("             Cycle ", i, n)
-                    update_weight(i, n, 0)
-                    print(graph)
-                    print("\n")
 
+                    update_weight(i, n, 0)
+
+# dijkstra: Any -> int
+# Implementa o algoritmo de dijkstra para menor caminho em um grafo.
 def dijkstra(u):
     distances = {v: inf-1 for v in graph}
     distances[u] = 0
@@ -84,55 +83,74 @@ def dijkstra(u):
 
     return distances
 
-print("\ngrafo 1")
-add_vertex('1')
-add_vertex('2')
-add_vertex('3')
-add_vertex('4')
-add_edge('1', '2', 5) 
-add_edge('2', '1', 10) 
-add_edge('3', '4', 8)
-add_edge('4', '3', 7)
-add_edge('2', '3', 6)
+# shortest_path_cost: Any, Any -> int
+# Obtém o custo do menor caminho entre uma origem e um destino no grafo.
+def shortest_path_cost(u, v):
+    shortest_path = dijkstra(u)
 
-verify_cycles(graph)
-
-edges = []
-for node in graph:
-    for vizinho in graph[node]:
-        edges.append((node, vizinho))
-
-for node in graph:
-    origin = node
-    shortest_path = dijkstra(origin)
-
+    cost = inf
     for destiny, distance in shortest_path.items():
-        if distance == (inf-1):
-            distance = "Nao e possivel entregar a carta"
-        print(f"Caminho mais curto de {origin} para {destiny}: {distance}")
+        if destiny == v:
+            cost = distance
+            if distance == (inf-1):
+                cost = "Nao e possivel entregar a carta"
     
-graph.clear()
+    return cost
 
-print("\ngrafo 2")
-add_vertex('1')
-add_vertex('2')
-add_vertex('3')
-add_edge('1', '2', 10)
-add_edge('2', '3', 1)
-add_edge('3', '2', 1)
+# ========== TESTES ==========
+# print("\ngrafo 1")
+# add_vertex('1')
+# add_vertex('2')
+# add_vertex('3')
+# add_vertex('4')
+# add_edge('1', '2', 5) 
+# add_edge('2', '1', 10) 
+# add_edge('3', '4', 8)
+# add_edge('4', '3', 7)
+# add_edge('2', '3', 6)
+# verify_cycles(graph)
 
-verify_cycles(graph)
 
-edges = []
-for node in graph:
-    for vizinho in graph[node]:
-        edges.append((node, vizinho))
+first_line = input()
 
-for node in graph:
-    origin = node
-    shortest_path = dijkstra(origin)
+while(first_line != "0 0"):
 
-    for destiny, distance in shortest_path.items():
-        if distance == (inf-1):
-            distance = "Nao e possivel entregar a carta"
-        print(f"Caminho mais curto de {origin} para {destiny}: {distance}")
+    N, E = first_line.split(" ")
+
+    count = 0
+    while (count < int(E)):
+        acordo = input()
+        X, Y, H = acordo.split(" ")
+        add_edge(X, Y, int(H)) 
+        count += 1
+
+    verify_cycles(graph)
+
+    K = int(input())
+
+    count = 0
+    results = []
+    while (count < K):
+        consulta = input()
+        O, D = consulta.split(" ")
+
+        print(shortest_path_cost(O, D))
+        # results.append(shortest_path_cost(O, D))
+
+        count += 1
+
+    # for node in graph:
+    #     origin = node
+    #     shortest_path = dijkstra(origin)
+
+    #     for destiny, distance in shortest_path.items():
+    #         if distance == (inf-1):
+    #             distance = "Nao e possivel entregar a carta"
+    #         print(f"Caminho mais curto de {origin} para {destiny}: {distance}")
+
+    # print(results)
+    print("\n")
+
+    graph.clear()
+
+    first_line = input()
