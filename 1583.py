22db9652412602a953f011db9982188185ecb29d
Leonardo Azzi Martins <leoazzim@gmail.com>
2024-02-04 15:37:49 -0300

    1583: Solução aceita, implementa DFS e representação matricial de mapa

--- 1583.py
+++ 1583.py
@@ -2,7 +2,6 @@
 # https://www.beecrowd.com.br/judge/pt/problems/view/1583
 
 from collections import defaultdict
-import heapq
 
 # Adiciona um vértice disconexo ao grafo se for um novo vértice
 # v: vértice
@@ -25,27 +24,31 @@ def update_weight(graph, u, v, new_weight):
 	if v in graph[u][0][0]:
 		graph[u][0][1] = new_weight
 
+# connect_cells
+# Compara vértices à esquerda e acima de um vértice de referência. Se os vértices
+# adjacentes pertencem ao grafo de células (A ou T), eles são conectados com o 
+# vértice de referência.
 def connect_cells(graph, x, y, mapa): 
-	# regras de conexão
-	vertex_1 = (x, y)
-	vertex_2 = (x-1, y)
+	
+	# Esquerda
+	vertex_ref = (x, y)
+	vertex_left = (x-1, y)
 
-	if vertex_1 in graph and vertex_2 in graph:
+	if vertex_ref in graph and vertex_left in graph:
+		add_bi_edge(graph, vertex_ref, vertex_left)
 
-		add_bi_edge(graph, vertex_1, vertex_2)
-		
-		#print(f"Célula ({vertex_1}): {mapa[x][y]} em Célula({vertex_2}: {mapa[x-1][y]})\n")
+	# Acima
+	vertex_ref = (x, y)
+	vertex_above = (x, y-1)
 
-	vertex_1 = (x, y)
-	vertex_2 = (x, y-1)
-
-	if vertex_1 in graph and vertex_2 in graph:
-		vertex_2 = (x, y-1)
-		add_bi_edge(graph, vertex_1, vertex_2)
-		#print(f"Célula ({vertex_1}): {mapa[x][y]} em Célula ({vertex_2}: {mapa[x][y-1]})\n")
+	if vertex_ref in graph and vertex_above in graph:
+		add_bi_edge(graph, vertex_ref, vertex_above)
 
 	return graph
 
+# contaminar
+# Utiliza DFS para buscar no mapa os vizinhos de vértices 'T' e substituir sua 
+# célula para 'T' (contaminar)
 def contaminar(grafo, vertice_inicio, map):
 	fronteira = []
 	visitados = set()
@@ -53,8 +56,6 @@ def contaminar(grafo, vertice_inicio, map):
 	fronteira.append(vertice_inicio)
 
 	while fronteira:
-		# print("fronteira:", fronteira)
-		# print("visitados:", visitados)
 		vertice = fronteira.pop()
 		if vertice not in visitados:
 			visitados.add(vertice)
@@ -89,7 +90,6 @@ while(first_input != "0 0"):
 		for col, cell in enumerate(line_input, start=0):
 			if cell == 'A' or cell == 'T':
 				if cell == 'T': T_coords.add((row,col))
-				# print(cell, row,col)
 				add_vertex(grafo, (row,col))
 				grafo = connect_cells(grafo, row, col, map)
 		row += 1
@@ -97,17 +97,16 @@ while(first_input != "0 0"):
 
 	# Visitar todos os nodos do grafo a partir de uma coordenada T
 	# Verificar se a próxima coordenada T já foi visitada
-	# Se não, visitar grafo da próxima coordenada
+	# Se não, visitar próxima coordenada
 	
 	conjunto_visitas = set()
 	for contaminant_xy in T_coords:
-		# print("contaminante: ", contaminant_xy)
-		# print("visitas já feitas", conjunto_visitas)
 
 		if contaminant_xy not in conjunto_visitas:
 			nodos_visitados = contaminar(grafo, contaminant_xy, map)
 			conjunto_visitas = conjunto_visitas.union(nodos_visitados)
 
+	# Saída
 	linha = []
 	for row in range(0, N):
 		linha = map[row]
